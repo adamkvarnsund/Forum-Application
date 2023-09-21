93813f69ca9c57b284837257e005a92279b2b3c3
Adam Kvarnsund <adam.kvarnsund17@gmail.com>
2023-09-21 23:22:11 +0200

    final commit

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const express = require('express'); // Import the Express.js library
+const bodyParser = require('body-parser'); // Middleware for parsing request bodies
+const fs = require('fs'); // File system module
 
-const app = express();
-const PORT = 3000;
+const app = express(); // Create an Express application
+const PORT = 3000; // Set the server's port
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Configure Express to use JSON body parsing
 
-app.use(express.static('public'));
-app.use('/src', express.static('src'));
+app.use(express.static('public')); // Serve static files from the 'public' directory
+app.use('/src', express.static('src')); // Serve static files from the 'src' directory
 
-let forumData = { posts: [] };
+let forumData = { posts: [] }; // Initialize an empty data structure for forum posts
 
+// Check if a JSON file ('data.json') exists and load its data if available
 if (fs.existsSync('data.json')) {
   forumData = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 }
 
+// Function to save the forum data to a JSON file
 function saveDataToFile() {
   fs.writeFileSync('data.json', JSON.stringify(forumData, null, 2), 'utf-8');
 }
+
+// Define routes and handlers for different API endpoints:
 
 // Get all posts
 app.get('/api/posts', (req, res) => {
@@ -29,6 +33,7 @@ app.get('/api/posts', (req, res) => {
 app.post('/api/posts', (req, res) => {
   const post = req.body;
   if (post) {
+    // Create a new post object with an ID, date, and comments array
     const date = new Date();
     const currentDate = `${date.getFullYear()}-${(date.getMonth() + 1)
       .toString()
@@ -40,9 +45,9 @@ app.post('/api/posts', (req, res) => {
       date: currentDate,
       comments: [],
     };
-    forumData.posts.push(newPost);
-    saveDataToFile();
-    res.json(newPost);
+    forumData.posts.push(newPost); // Add the new post to the data
+    saveDataToFile(); // Save the updated data to the JSON file
+    res.json(newPost); // Respond with the created post
   } else {
     res.status(400).json({ error: 'Invalid post data' });
   }
@@ -53,32 +58,36 @@ app.put('/api/posts/:postId', (req, res) => {
   const postId = parseInt(req.params.postId);
   const newContent = req.body.content;
 
+  // Find the post with the given ID
   const post = forumData.posts.find((p) => p.id === postId);
 
   if (!post) {
     return res.status(404).json({ error: 'Post not found' });
   }
 
+  // Update the post's content
   post.content = newContent;
-  saveDataToFile();
+  saveDataToFile(); // Save the updated data to the JSON file
 
-  res.json(post);
+  res.json(post); // Respond with the updated post
 });
 
 // Delete a post
 app.delete('/api/posts/:postId', (req, res) => {
   const postId = parseInt(req.params.postId);
 
+  // Find the index of the post with the given ID
   const postIndex = forumData.posts.findIndex((p) => p.id === postId);
 
   if (postIndex === -1) {
     return res.status(404).json({ error: 'Post not found' });
   }
 
+  // Remove the post from the data
   forumData.posts.splice(postIndex, 1);
-  saveDataToFile();
+  saveDataToFile(); // Save the updated data to the JSON file
 
-  res.status(204).send();
+  res.status(204).send(); // Respond with a 204 status (No Content)
 });
 
 // Add a comment to a post
@@ -86,20 +95,22 @@ app.post('/api/posts/:postId/comments', (req, res) => {
   const postId = parseInt(req.params.postId);
   const commentContent = req.body.content;
 
+  // Find the post with the given ID
   const post = forumData.posts.find((p) => p.id === postId);
 
   if (!post) {
     return res.status(404).json({ error: 'Post not found' });
   }
 
+  // Create a new comment
   const newComment = {
     id: post.comments.length + 1,
     content: commentContent,
   };
-  post.comments.push(newComment);
-  saveDataToFile();
+  post.comments.push(newComment); // Add the new comment to the post's comments
+  saveDataToFile(); // Save the updated data to the JSON file
 
-  res.json(newComment);
+  res.json(newComment); // Respond with the created comment
 });
 
 // Edit a comment
@@ -108,22 +119,25 @@ app.put('/api/posts/:postId/comments/:commentId', (req, res) => {
   const commentId = parseInt(req.params.commentId);
   const newContent = req.body.content;
 
+  // Find the post with the given ID
   const post = forumData.posts.find((p) => p.id === postId);
 
   if (!post) {
     return res.status(404).json({ error: 'Post not found' });
   }
 
+  // Find the comment with the given ID
   const comment = post.comments.find((c) => c.id === commentId);
 
   if (!comment) {
     return res.status(404).json({ error: 'Comment not found' });
   }
 
+  // Update the comment's content
   comment.content = newContent;
-  saveDataToFile();
+  saveDataToFile(); // Save the updated data to the JSON file
 
-  res.json(comment);
+  res.json(comment); // Respond with the updated comment
 });
 
 // Delete a comment
@@ -131,26 +145,28 @@ app.delete('/api/posts/:postId/comments/:commentId', (req, res) => {
   const postId = parseInt(req.params.postId);
   const commentId = parseInt(req.params.commentId);
 
+  // Find the post with the given ID
   const post = forumData.posts.find((p) => p.id === postId);
 
   if (!post) {
     return res.status(404).json({ error: 'Post not found' });
   }
 
+  // Find the index of the comment with the given ID
   const commentIndex = post.comments.findIndex((c) => c.id === commentId);
 
   if (commentIndex === -1) {
     return res.status(404).json({ error: 'Comment not found' });
   }
 
+  // Remove the comment from the post's comments
   post.comments.splice(commentIndex, 1);
-  saveDataToFile();
+  saveDataToFile(); // Save the updated data to the JSON file
 
-  res.status(204).send();
+  res.status(204).send(); // Respond with a 204 status (No Content)
 });
 
-// Define other routes for editing and deleting posts/comments...
-
+// Start the Express server on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- src/app.js
+++ src/app.js
@@ -1,263 +1,276 @@
 // Data
 const forumData = {
-  posts: [],
-};
-
-// Function to toggle comments visibility
-function toggleComments(postId) {
-  const commentsDiv = document.getElementById(`comments_${postId}`);
-  const showCommentsButton = document.getElementById(`showComments_${postId}`);
-
-  if (commentsDiv.style.display === 'none' || commentsDiv.style.display === '') {
+    posts: [],
+  };
+  
+  // Function to toggle comments visibility
+  function toggleComments(postId) {
+    // Get references to the comments div and show/hide button by postId
+    const commentsDiv = document.getElementById(`comments_${postId}`);
+    const showCommentsButton = document.getElementById(`showComments_${postId}`);
+  
+    // Toggle the display property of commentsDiv and update the button text
+    if (commentsDiv.style.display === 'none' || commentsDiv.style.display === '') {
       commentsDiv.style.display = 'block';
       showCommentsButton.textContent = 'Hide Comments';
-  } else {
+    } else {
       commentsDiv.style.display = 'none';
       showCommentsButton.textContent = 'Show Comments';
+    }
   }
-}
-
-// Add an event listener to the sort dropdown menu
-const sortSelect = document.getElementById('sortSelect');
-
-let currentSortOrder = 'newest'; // Set the initial default sort order
-
-sortSelect.addEventListener('change', () => {
-  const selectedValue = sortSelect.value;
-  currentSortOrder = selectedValue; // Update the current sort order
-  renderPosts(currentSortOrder);
-});
-
-// Function to render posts
-async function renderPosts(sortOrder) {
-  try {
-      // Fetch posts data from the server
+  
+  // Add an event listener to the sort dropdown menu
+  const sortSelect = document.getElementById('sortSelect');
+  
+  let currentSortOrder = 'newest'; // Set the initial default sort order
+  
+  sortSelect.addEventListener('change', () => {
+    const selectedValue = sortSelect.value;
+    currentSortOrder = selectedValue; // Update the current sort order
+    renderPosts(currentSortOrder);
+  });
+  
+  // Function to render posts
+  async function renderPosts(sortOrder) {
+    try {
+      // Fetch posts data from the server with the specified sort order
       const response = await fetch(`/api/posts?sort=${currentSortOrder}`);
       if (!response.ok) {
-          throw new Error('Error fetching data');
+        throw new Error('Error fetching data');
       }
       const posts = await response.json();
-
-      // Sort the posts array based on the sortOrder
+  
+      // Sort the posts array based on the selected sortOrder
+      // Different sorting options: alphabetical, reverse-alphabetical, newest
       if (sortOrder === 'alphabetical') {
-          posts.sort((a, b) => a.header.localeCompare(b.header));
+        posts.sort((a, b) => a.header.localeCompare(b.header));
       }
       if (sortOrder === 'reverse-alphabetical') {
-          posts.sort((a, b) => a.header.localeCompare(b.header));
-          posts.reverse();
-
+        posts.sort((a, b) => a.header.localeCompare(b.header));
+        posts.reverse();
       }
       if (sortOrder === 'newest') {
         posts.reverse();
       }
-
+  
+      // Get a reference to the postList element and clear its contents
       const postList = document.getElementById('postList');
       postList.innerHTML = '';
-
+  
+      // Iterate through the posts and create HTML elements for each post
       posts.forEach((post) => {
-          const postDiv = document.createElement('div');
-          postDiv.className = 'post';
-          postDiv.innerHTML = `
-              <div class="post-info">
-                  <p class="post-date">${post.date}</p>
-                  <p class="post-header" id="post_${post.id}">${post.header}</p>
-              </div>
-              <p class="post-content" id="post_${post.id}">${post.content}</p>
-              <button class="comment-button" onclick="addComment(${post.id})">Add Comment</button>
-              <button class="edit-button" onclick="editPost(${post.id})">Edit Post</button>
-              <button class="delete-button" onclick="deletePost(${post.id})">Delete Post</button>
-              <div id="comments_${post.id}"></div>
-          `;
-
-          const commentsDiv = postDiv.querySelector(`#comments_${post.id}`);
-          const commentCount = post.comments.length;
-
-          // Check if there's at least one comment to display
-          if (commentCount > 0) {
-              // Create a div to hold comments other than the last one
-              const hiddenCommentsDiv = document.createElement('div');
-              hiddenCommentsDiv.style.display = 'none'; // Initially hide this div
-              hiddenCommentsDiv.className = 'hidden-comments';
-
-              post.comments.forEach((comment, index) => {
-                  const commentDiv = document.createElement('div');
-                  commentDiv.className = 'comment';
-                  commentDiv.innerHTML = `
-                      <p><i class="comment-tag">Comment</i></p>
-                      <p id="comment_${comment.id}">${comment.content}</p>
-                      <button class="edit-comment-button" onclick="editComment(${post.id}, ${comment.id})">Edit</button>
-                      <button class="delete-comment-button" onclick="deleteComment(${post.id}, ${comment.id})">Delete</button>
-                  `;
-
-                  if (index === commentCount - 1) {
-                      // The last comment remains under the post
-                      commentsDiv.appendChild(commentDiv);
-                  } else {
-                      // Other comments go in the hidden div
-                      hiddenCommentsDiv.appendChild(commentDiv);
-                  }
-              });
-
-              if (commentCount > 1) {
-                  const showHiddenCommentsButton = document.createElement('paragraph');
-                  showHiddenCommentsButton.textContent = 'Show more comments' + " " + "(" + (commentCount-1) +")";
-                  showHiddenCommentsButton.className = 'show-comments-button';
-
-                  showHiddenCommentsButton.addEventListener('click', () => {
-                      if (hiddenCommentsDiv.style.display === 'none') {
-                          hiddenCommentsDiv.style.display = 'block';
-                          showHiddenCommentsButton.textContent = 'Hide Comments';
-                      } else {
-                          hiddenCommentsDiv.style.display = 'none';
-                          showHiddenCommentsButton.textContent = 'Show more comments' + " " + "(" + (commentCount-1) +")";
-                      }
-                  });
-
-                  commentsDiv.appendChild(showHiddenCommentsButton);
-                  commentsDiv.appendChild(hiddenCommentsDiv);
+        const postDiv = document.createElement('div');
+        postDiv.className = 'post';
+        postDiv.innerHTML = `
+          <div class="post-info">
+            <p class="post-date">${post.date}</p>
+            <p class="post-header" id="post_${post.id}">${post.header}</p>
+          </div>
+          <p class="post-content" id="post_${post.id}">${post.content}</p>
+          <button class="comment-button" onclick="addComment(${post.id})">Add Comment</button>
+          <button class="edit-button" onclick="editPost(${post.id})">Edit Post</button>
+          <button class="delete-button" onclick="deletePost(${post.id})">Delete Post</button>
+          <div id="comments_${post.id}"></div>
+        `;
+  
+        const commentsDiv = postDiv.querySelector(`#comments_${post.id}`);
+        const commentCount = post.comments.length;
+  
+        // Check if there are comments to display
+        if (commentCount > 0) {
+          // Create a div to hold comments other than the last one
+          const hiddenCommentsDiv = document.createElement('div');
+          hiddenCommentsDiv.style.display = 'none'; // Initially hide this div
+          hiddenCommentsDiv.className = 'hidden-comments';
+  
+          // Iterate through comments and create HTML elements for each
+          post.comments.forEach((comment, index) => {
+            const commentDiv = document.createElement('div');
+            commentDiv.className = 'comment';
+            commentDiv.innerHTML = `
+              <p><i class="comment-tag">Comment</i></p>
+              <p id="comment_${comment.id}">${comment.content}</p>
+              <button class="edit-comment-button" onclick="editComment(${post.id}, ${comment.id})">Edit</button>
+              <button class="delete-comment-button" onclick="deleteComment(${post.id}, ${comment.id})">Delete</button>
+            `;
+  
+            // Append comments to the appropriate div (last one under the post, others in hidden div)
+            if (index === commentCount - 1) {
+              commentsDiv.appendChild(commentDiv);
+            } else {
+              hiddenCommentsDiv.appendChild(commentDiv);
+            }
+          });
+  
+          // Show more comments button for posts with multiple comments
+          if (commentCount > 1) {
+            const showHiddenCommentsButton = document.createElement('paragraph');
+            showHiddenCommentsButton.textContent = 'Show more comments' + " " + "(" + (commentCount - 1) + ")";
+            showHiddenCommentsButton.className = 'show-comments-button';
+  
+            showHiddenCommentsButton.addEventListener('click', () => {
+              if (hiddenCommentsDiv.style.display === 'none') {
+                hiddenCommentsDiv.style.display = 'block';
+                showHiddenCommentsButton.textContent = 'Hide Comments';
+              } else {
+                hiddenCommentsDiv.style.display = 'none';
+                showHiddenCommentsButton.textContent = 'Show more comments' + " " + "(" + (commentCount - 1) + ")";
               }
+            });
+  
+            commentsDiv.appendChild(showHiddenCommentsButton);
+            commentsDiv.appendChild(hiddenCommentsDiv);
           }
-
-          postList.appendChild(postDiv);
+        }
+  
+        postList.appendChild(postDiv); // Append the post to the postList
       });
-  } catch (error) {
+    } catch (error) {
       console.error('Error rendering posts:', error);
+    }
   }
-}
-
-// Function to add a new post
-async function addPost() {
-  const postHeader = document.getElementById('postHeader').value;
-  const postContent = document.getElementById('postContent').value;
-  if (postContent && postHeader) {
+  
+  // Function to add a new post
+  async function addPost() {
+    // Get the post header and content from user input
+    const postHeader = document.getElementById('postHeader').value;
+    const postContent = document.getElementById('postContent').value;
+    if (postContent && postHeader) {
       const post = {
-          header: postHeader,
-          content: postContent,
+        header: postHeader,
+        content: postContent,
       };
       try {
-          const response = await fetch('/api/posts', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(post),
-          });
-          if (!response.ok) {
-              throw new Error('Error adding post');
-          }
-          renderPosts(currentSortOrder); // Render posts with the current sort order
-          document.getElementById('postHeader').value = '';
-          document.getElementById('postContent').value = '';
+        // Send a POST request to add the new post
+        const response = await fetch('/api/posts', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(post),
+        });
+        if (!response.ok) {
+          throw new Error('Error adding post');
+        }
+        renderPosts(currentSortOrder); // Render posts with the current sort order
+        document.getElementById('postHeader').value = '';
+        document.getElementById('postContent').value = '';
       } catch (error) {
-          console.error('Error adding post:', error);
+        console.error('Error adding post:', error);
       }
+    }
   }
-}
-
-// Function to edit a post
-async function editPost(postId) {
-  const newContent = prompt('Edit your post:');
-  if (newContent !== null) {
+  
+  // Function to edit a post
+  async function editPost(postId) {
+    const newContent = prompt('Edit your post:');
+    if (newContent !== null) {
       try {
-          const response = await fetch(`/api/posts/${postId}`, {
-              method: 'PUT',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ content: newContent }),
-          });
-          if (!response.ok) {
-              throw new Error('Error editing post');
-          }
-          renderPosts(currentSortOrder); // Render posts with the current sort order
+        // Send a PUT request to edit the post's content
+        const response = await fetch(`/api/posts/${postId}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ content: newContent }),
+        });
+        if (!response.ok) {
+          throw new Error('Error editing post');
+        }
+        renderPosts(currentSortOrder); // Render posts with the current sort order
       } catch (error) {
-          console.error('Error editing post:', error);
+        console.error('Error editing post:', error);
       }
+    }
   }
-}
-
-// Function to delete a post
-async function deletePost(postId) {
-  const confirmed = confirm('Are you sure you want to delete this post?');
-  if (confirmed) {
+  
+  // Function to delete a post
+  async function deletePost(postId) {
+    const confirmed = confirm('Are you sure you want to delete this post?');
+    if (confirmed) {
       try {
-          const response = await fetch(`/api/posts/${postId}`, {
-              method: 'DELETE',
-          });
-          if (!response.ok) {
-              throw new Error('Error deleting post');
-          }
-          renderPosts(currentSortOrder); // Render posts with the current sort order
+        // Send a DELETE request to delete the post
+        const response = await fetch(`/api/posts/${postId}`, {
+          method: 'DELETE',
+        });
+        if (!response.ok) {
+          throw new Error('Error deleting post');
+        }
+        renderPosts(currentSortOrder); // Render posts with the current sort order
       } catch (error) {
-          console.error('Error deleting post:', error);
+        console.error('Error deleting post:', error);
       }
+    }
   }
-}
-
-// Function to add a comment to a post
-async function addComment(postId) {
-  const commentContent = prompt('Enter your comment:');
-  if (commentContent) {
+  
+  // Function to add a comment to a post
+  async function addComment(postId) {
+    const commentContent = prompt('Enter your comment:');
+    if (commentContent) {
       const comment = {
-          content: commentContent,
+        content: commentContent,
       };
       try {
-          const response = await fetch(`/api/posts/${postId}/comments`, {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(comment),
-          });
-          if (!response.ok) {
-              throw new Error('Error adding comment');
-          }
-          renderPosts(currentSortOrder); // Render posts with the current sort order
+        // Send a POST request to add a comment to the post
+        const response = await fetch(`/api/posts/${postId}/comments`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(comment),
+        });
+        if (!response.ok) {
+          throw new Error('Error adding comment');
+        }
+        renderPosts(currentSortOrder); // Render posts with the current sort order
       } catch (error) {
-          console.error('Error adding comment:', error);
+        console.error('Error adding comment:', error);
       }
+    }
   }
-}
-
-// Function to edit a comment
-async function editComment(postId, commentId) {
-  const newContent = prompt('Edit your comment:');
-  if (newContent !== null) {
+  
+  // Function to edit a comment
+  async function editComment(postId, commentId) {
+    const newContent = prompt('Edit your comment:');
+    if (newContent !== null) {
       try {
-          const response = await fetch(`/api/posts/${postId}/comments/${commentId}`, {
-              method: 'PUT',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ content: newContent }),
-          });
-          if (!response.ok) {
-              throw new Error('Error editing comment');
-          }
-          renderPosts(currentSortOrder); // Render posts with the current sort order
+        // Send a PUT request to edit the comment's content
+        const response = await fetch(`/api/posts/${postId}/comments/${commentId}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ content: newContent }),
+        });
+        if (!response.ok) {
+          throw new Error('Error editing comment');
+        }
+        renderPosts(currentSortOrder); // Render posts with the current sort order
       } catch (error) {
-          console.error('Error editing comment:', error);
+        console.error('Error editing comment:', error);
       }
+    }
   }
-}
-
-// Function to delete a comment
-async function deleteComment(postId, commentId) {
-  const confirmed = confirm('Are you sure you want to delete this comment?');
-  if (confirmed) {
+  
+  // Function to delete a comment
+  async function deleteComment(postId, commentId) {
+    const confirmed = confirm('Are you sure you want to delete this comment?');
+    if (confirmed) {
       try {
-          const response = await fetch(`/api/posts/${postId}/comments/${commentId}`, {
-              method: 'DELETE',
-          });
-          if (!response.ok) {
-              throw new Error('Error deleting comment');
-          }
-          renderPosts(currentSortOrder); // Render posts with the current sort order
+        // Send a DELETE request to delete the comment
+        const response = await fetch(`/api/posts/${postId}/comments/${commentId}`, {
+          method: 'DELETE',
+        });
+        if (!response.ok) {
+          throw new Error('Error deleting comment');
+        }
+        renderPosts(currentSortOrder); // Render posts with the current sort order
       } catch (error) {
-          console.error('Error deleting comment:', error);
+        console.error('Error deleting comment:', error);
       }
+    }
   }
-}
-
-// Initial rendering of posts
-renderPosts();
+  
+  // Initial rendering of posts
+  renderPosts();
+  
